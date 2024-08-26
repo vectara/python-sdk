1cdb0ae55462c82cbf4abef6ba5114f8beebcd24
David Levy <david@vectara.com>
2024-08-26 10:47:54 +1000

    Added YAML and required types for poetry with initial factory test as secondary construction method

---
+++ int_tests/test_factory.py
@@ -0,0 +1,31 @@
+import unittest
+import logging
+
+from vectara.factory import Factory
+from vectara.corpora.client import CorporaClient
+
+class FactoryTest(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+    def test_list_corpora_filter(self):
+
+        self.logger.info("Starting test")
+
+        client = Factory().build()
+
+        corpora_client : CorporaClient = client.corpora
+
+
+        def list_corpora_gen():
+            response = corpora_client.list(filter="company_names")
+            for item in response:
+                yield item
+
+        for corpus in list_corpora_gen():
+            self.logger.info(f"Found {corpus.name}")
+
+
+
