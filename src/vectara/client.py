18d7a08bbc9c4606bffc4c4fd8bd080813206a08
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-09-30 19:35:15 +0000

    SDK regeneration

--- src/vectara/client.py
+++
@@ -1,7 +0,0 @@
-from .base_client import BaseVectara, AsyncBaseVectara
-
-class Vectara(BaseVectara): 
-    pass
-
-class AsyncVectara(AsyncBaseVectara): 
-    pass
--- tests/custom/test_client.py
+++ tests/custom/test_client.py
@@ -2,7 +2,6 @@ import pytest
 
 
 # Get started with writing tests with pytest at https://docs.pytest.org
-# Must have 1 file in package "custom" or build will break!!
 @pytest.mark.skip(reason="Unimplemented")
 def test_client() -> None:
     assert True == True
